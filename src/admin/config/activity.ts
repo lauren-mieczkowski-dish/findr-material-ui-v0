4654941c5b8127a4c74434fe95dbaf0bfd312042
lauren-mieczkowski <121053213+lauren-mieczkowski@users.noreply.github.com>
2023-11-28 13:56:30 -0700

    Delete src directory

--- src/admin/config/activity.ts
+++
@@ -1,7 +0,0 @@
-export const logKeys: { [key: string]: string } = {
-  eventAdded: "profile.activity.logs.eventAdded",
-  eventUpdated: "profile.activity.logs.eventUpdated",
-  userAdded: "profile.activity.logs.eventAdded",
-  deviceDeleted: "profile.activity.logs.deviceDeleted",
-  deviceUpdated: "profile.activity.logs.deviceUpdated",
-};
